07e312dcc35e3e41f90b4882ef4c0f5e10ff4731
Joonas88 <56264853+Joonas88@users.noreply.github.com>
2021-01-19 14:41:51 +0200

    Initial commit

---
+++ src/main/java/otp/group6/AudioEditor/App.java
@@ -0,0 +1,13 @@
+package otp.group6.AudioEditor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
